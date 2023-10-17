5176aa3c85f4367a27198013360772343c33b91f
LAMILEYMILLS <140696294+LAMILEYMILLS@users.noreply.github.com>
2023-10-17 23:05:49 +0100

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -4,6 +4,7 @@ void print_buffer(char buffer[], int *buff_ind);
 /**
  * _printf - Printf function
  * @format: format
+ * by Thomas and Keziah
  * Return: Printed chars
  */
 int _printf(const char *format, ...)
@@ -24,8 +25,7 @@ int _printf(const char *format, ...)
 		{
 			buffer[buff_ind++] = format[i];
 			if (buff_ind == BUFF_SIZE)
-				
-print_buffer(buffer, &buff_ind);
+				print_buffer(buffer, &buff_ind);
 			/* write(1, &format[i], 1);*/
 			printed_chars++;
 		}
